26708429af063b346f07aed010fcd74add4f874e
This is Rude <dbauman1@swarthmore.edu>
2017-12-31 05:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function iterativeLog(array){
 function iterate(callback){
   var newarray = [6,1,4]
   newarray.forEach(callback)
+  return newarray
 }
 function doToArray(array,callback){
   array.forEach(callback)
